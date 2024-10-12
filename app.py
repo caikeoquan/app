1b02c75626509f9f54dd50b4e72cf73d42e2eda5
caikeoquan <77910946+caikeoquan@users.noreply.github.com>
2024-10-12 23:12:17 +0700

    Add files via upload

---
+++ app.py
@@ -0,0 +1,37 @@
+from flask import Flask, jsonify
+from web3 import Web3
+
+# Initialize Flask app
+app = Flask(__name__)
+
+# Connect to Ethereum Node (use Infura or your own node)
+infura_url = "https://rpc.ankr.com/eth"
+web3 = Web3(Web3.HTTPProvider(infura_url))
+
+# Token contract address and ABI (replace with your token's address and ABI)
+contract_address = Web3.to_checksum_address("0x488A82038Bc28Cead43a799DC6a40c03BF007aee")
+contract_abi = [
+    {
+        "constant": True,
+        "inputs": [],
+        "name": "totalSupply",
+        "outputs": [{"name": "", "type": "uint256"}],
+        "payable": False,
+        "stateMutability": "view",
+        "type": "function",
+    }
+]
+
+# Initialize contract instance
+contract = web3.eth.contract(address=contract_address, abi=contract_abi)
+
+@app.route('/total-supply', methods=['GET'])
+def get_total_supply():
+    try:
+        total_supply = int(int(contract.functions.totalSupply().call())/10**9)
+        return jsonify(total_supply)
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5500)
