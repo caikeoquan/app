78ef619c1baf17cb4e03f2df4d15ad688b4e2330
caikeoquan <77910946+caikeoquan@users.noreply.github.com>
2024-10-12 23:23:31 +0700

    Update app.py

--- app.py
+++ app.py
@@ -33,5 +33,13 @@ def get_total_supply():
     except Exception as e:
         return jsonify({"error": str(e)}), 500
 
+@app.route('/cir-supply', methods=['GET'])
+def get_cir_supply():
+    try:
+        cir_supply = 90000000
+        return jsonify(cir_supply)
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5500)
